913f813a3936c3398694c4166f8388adc69f66a9
neronix17 <39186138+neronix17@users.noreply.github.com>
2024-03-18 11:32:20 +0000

    1.5 Stuff

---
+++ 1.5/Source/OuterRimDiversity/OuterRimDiversityMod.cs
@@ -0,0 +1,44 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+
+namespace OuterRimDiversity
+{
+    public class OuterRimDiversityMod : Mod
+    {
+        public static OuterRimDiversityMod mod;
+
+        internal static string VersionDir => Path.Combine(mod.Content.ModMetaData.RootDir.FullName, "Version.txt");
+        public static string CurrentVersion { get; private set; }
+
+        public OuterRimDiversityMod(ModContentPack content) : base(content)
+        {
+            mod = this;
+
+            Version version = Assembly.GetExecutingAssembly().GetName().Version;
+            CurrentVersion = $"{version.Major}.{version.Minor}.{version.Build}";
+
+            LogUtil.LogMessage($"{CurrentVersion} ::");
+
+            File.WriteAllText(VersionDir, CurrentVersion);
+
+            Harmony harmony = new Harmony("Neronix17.OuterRimDiversity.RimWorld");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+
+        //public override string SettingsCategory() => "Outer Rim - Galactic Diversity";
+
+        //public override void DoSettingsWindowContents(Rect inRect)
+        //{
+        //    base.DoSettingsWindowContents(inRect);
+        //}
+    }
+}
