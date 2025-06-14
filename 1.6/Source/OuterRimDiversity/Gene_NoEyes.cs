7629a417669f8a74490956a200eb0ab60840f17d
Neronix17 <39186138+neronix17@users.noreply.github.com>
2025-06-14 15:30:54 +0100

    Initial 1.6 Update

---
+++ 1.6/Source/OuterRimDiversity/Gene_NoEyes.cs
@@ -0,0 +1,33 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+
+namespace OuterRimDiversity
+{
+    public class Gene_NoEyes : Gene
+    {
+        public override void PostAdd()
+        {
+            base.PostAdd();
+            if (Active)
+            {
+                foreach (BodyPartRecord part in pawn.health.hediffSet.GetNotMissingParts())
+                {
+                    if(part.def == BodyPartDefOf.Eye)
+                    {
+                        Hediff_MissingPart hediff = (Hediff_MissingPart)HediffMaker.MakeHediff(ORDivDefOf.OuterRim_VestigialEye, pawn);
+                        hediff.lastInjury = ORDivDefOf.OuterRim_Vestigial;
+                        hediff.part = part;
+                        hediff.IsFresh = false;
+                        pawn.health.AddHediff(hediff);
+                    }
+                }
+            }
+        }
+    }
+}
